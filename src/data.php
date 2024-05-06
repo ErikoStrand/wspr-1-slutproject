128a421468a790f06691de2f5ffa2711aa7b8517
Erik <erikstrandberg153@gmail.com>
2024-05-06 12:44:06 +0200

    added insert for general, movie, show.
    yes yes yes now you can upload now i just have to add a check to see if they have uploaded and if they have they can "update their ratings",
    basically just delete and insert again? maybe check if any new if thats possible.

--- src/data.php
+++ src/data.php
@@ -4,8 +4,81 @@ include "config/database.php";
 if (session_status() === PHP_SESSION_NONE) {
   session_start();
 }
-function insertData($conn, $array, $name) {
+function insertMovie($conn, $array) {
+  if(isset($_SESSION["userID"])) {
+    $id = $_SESSION["userID"];
+    $sql = "INSERT INTO moviedata (userID, totalMedia, totalWatchtimeMinutes, totalWatchtimeHours, totalDirectors, averageRating, averageRatingIMDB)
+    VALUES (?, ?, ?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sssssss",
+                    $id,
+                    $array["totalMedia"],
+                    $array["totalWatchtimeMinutes"],
+                    $array["totalWatchtimeHours"],
+                    $array["totalDirectors"],
+                    $array["averageRating"],
+                    $array["averageRatingIMDB"]);
+    if (!$stmt->execute()) {
+      echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    } else {
+      echo "Record inserted successfully.";
+    }
 
+    $stmt->close();
+  } else {
+    return "errrr";
+  }
+}
+
+function insertGeneral($conn, $array) {
+  if(isset($_SESSION["userID"])) {
+    $id = $_SESSION["userID"];
+    $sql = "INSERT INTO generaldata (userID, highestStreak, currentStreak, totalMedia, totalAverageRating, averageMediaPerMonth, averageMediaPerWeek, weeksSinceStart)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssssssss",
+                    $id,
+                    $array["streak"]["highestStreak"],
+                    $array["streak"]["currentStreak"],
+                    $array["totalMedia"],
+                    $array["totalAverageRating"],
+                    $array["averageMediaPerMonth"],
+                    $array["averageMediaPerWeek"],
+                    $array["weeksSinceStart"]);
+    if (!$stmt->execute()) {
+      echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    } else {
+      echo "Record inserted successfully.";
+    }
+
+    $stmt->close();
+  } else {
+    return "errrr";
+  }
+}
+function insertShow($conn, $array) {
+  if(isset($_SESSION["userID"])) {
+    $id = $_SESSION["userID"];
+    $sql = "INSERT INTO showdata (userID, totalMedia, totalWatchtimeMinutes, totalWatchtimeHours, averageRating, averageRatingIMDB)
+    VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssssss",
+                    $id,
+                    $array["totalMedia"],
+                    $array["totalWatchtimeMinutes"],
+                    $array["totalWatchtimeHours"],
+                    $array["averageRating"],
+                    $array["averageRatingIMDB"]);
+    if (!$stmt->execute()) {
+      echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    } else {
+      echo "Record inserted successfully.";
+    }
+
+    $stmt->close();
+  } else {
+    return "errrr";
+  }
 }
 
 function cleanData($array) {
@@ -31,19 +104,18 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   // Retrieve the data sent from the client
   if (isset($_POST["movies"])) {
     $movies = cleanData((array)json_decode($_POST["movies"]));
-    $_SESSION["movies"] = $movies;
+    insertMovie($conn, $movies);
     echo "done movies";
   }
   if (isset($_POST["shows"])) {
     $shows = cleanData((array)json_decode($_POST["shows"]));
-    $_SESSION["shows"] = $shows;
+    insertShow($conn, $shows);
     echo "done shows";
 
   }
   if (isset($_POST["generalData"])) {
     $generalData = cleanData((array)json_decode($_POST["generalData"]));
-    $_SESSION["generalData"] = $generalData;
-    print_r($generalData);
+    insertGeneral($conn, $generalData);
     echo "done general";
   }
 

f92835e4d704f56d7ba44e21a856e51524b80f68
Erik Strandberg <erikstrandberg153@gmail.com>
2024-05-12 15:08:18 +0200

    infinite replies now work :)

--- src/profile.php
+++ src/profile.php
@@ -223,7 +223,7 @@ function followPerson($profileUserID, $conn) {
     <!--singular post-->
     <?php elseif($thoughtActivated && doesPostExist($postID, $conn, $profileUserID)): ?>
       <?php $singlePost = getUserPost($postID, $conn);?>
-      <div id="mainsingularcontent" class="flex flex-col gap-2 font-archivo border-b-2 border-zinc-800">
+      <div id="mainsingularcontent" class="flex flex-col gap-2 font-archivo border-b-[1px] border-zinc-800">
         <nav class="flex flex-row gap-4 font-archivo font-bold text-2xl items-center relative -left-2 mb-4">
           <a href="javascript:history.back()" class="rounded-full hover:bg-zinc-800 duration-200 ease-in-out p-2">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="28px" height="28px" class="fill-stone-200"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
@@ -237,7 +237,7 @@ function followPerson($profileUserID, $conn) {
         <p id="thoughttext" class="font-light break-words"><?php echo $singlePost["text"];?></p>
         <h3><?php echo convertUnixToFormattedDate($singlePost["postTime"]);?></h3>
         <!--likes and todo comments-->
-        <div id="postNavbar" class="flex flex-row justify-between font-mono border-y-2 border-zinc-800 p-1">
+        <div id="postNavbar" class="flex flex-row justify-between font-mono border-b-[1px] border-zinc-800 p-1">
           <!-- will control comments unsure how to do it, maybe new window?-->
           <form class="py-1"><button class="flex flex-row gap-1 items-center"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20px" height="20px" class="fill-stone-200"><path d="M123.6 391.3c12.9-9.4 29.6-11.8 44.6-6.4c26.5 9.6 56.2 15.1 87.8 15.1c124.7 0 208-80.5 208-160s-83.3-160-208-160S48 160.5 48 240c0 32 12.4 62.8 35.7 89.2c8.6 9.7 12.8 22.5 11.8 35.5c-1.4 18.1-5.7 34.7-11.3 49.4c17-7.9 31.1-16.7 39.4-22.7zM21.2 431.9c1.8-2.7 3.5-5.4 5.1-8.1c10-16.6 19.5-38.4 21.4-62.9C17.7 326.8 0 285.1 0 240C0 125.1 114.6 32 256 32s256 93.1 256 208s-114.6 208-256 208c-37.1 0-72.3-6.4-104.1-17.9c-11.9 8.7-31.3 20.6-54.3 30.6c-15.1 6.6-32.3 12.6-50.1 16.1c-.8 .2-1.6 .3-2.4 .5c-4.4 .8-8.7 1.5-13.2 1.9c-.2 0-.5 .1-.7 .1c-5.1 .5-10.2 .8-15.3 .8c-6.5 0-12.3-3.9-14.8-9.9c-2.5-6-1.1-12.8 3.4-17.4c4.1-4.2 7.8-8.7 11.3-13.5c1.7-2.3 3.3-4.6 4.8-6.9c.1-.2 .2-.3 .3-.5z"/></svg><div class="text-lg"><?php echo 0; ?></div></button></form>
           <!-- controls likes and unlikes-->
